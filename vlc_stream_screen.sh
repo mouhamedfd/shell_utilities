c6240d7b899c3bf4ccb4ad0c18fc7434676e54d4
Mouhamed Fadel DIAGANA <mouhamedfd@gmail.com>
2020-01-07 22:28:45 +0000

    modifié :         vlc_stream_screen.sh correct default options

--- vlc_stream_screen.sh
+++ vlc_stream_screen.sh
@@ -3,7 +3,7 @@
 # inpired by : https://superuser.com/questions/335885/stream-desktop-using-vlc
 # mod by mouhamedfd
 #
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ;then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] ;then
  echo this script use vlc media player to stream your desktop over lan
  echo $0 ip_adress port width height
 else
@@ -11,7 +11,7 @@ ip_adress=$1
 port=$2
 width=$3
 height=$4
-cvlc screen:// :screen-fps=30 :screen-caching=100 --sout '#transcode{vcodec=h264,vb=4096,acodec=mpga,ab=256,scale=1,width=1280,height=800‌​}:standard{access=http, mux=ts, dst=192.168.2.139:8085}'
+cvlc screen:// :screen-fps=30 :screen-caching=100 --sout "#transcode{vcodec=h264,vb=4096,acodec=mpga,ab=256,scale=1,width=$3,height=$4‌​}:standard{access=http, mux=ts, dst=$1:$2}"
 fi
 
 
