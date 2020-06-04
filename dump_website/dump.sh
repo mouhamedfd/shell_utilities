c8c67b2ca4e67a4094c137fb95d3eb4079786ac0
Mouhamed Fadel DIAGANA <mouhamedfd@gmail.com>
2020-06-04 18:20:03 +0000

    modifié :         dump_database/dump.sh 	nouveau fichier : dump_website/dump.sh Improve dump script, add dump website

--- dump_website/dump.sh
+++ dump_website/dump.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#script to export database to a cloud service using rclone ---> https://rclone.org/
+#script to backup Website or App to a cloud service using rclone ---> https://rclone.org/
 #This script can be use for automated database backup with cron ---> https://man7.org/linux/man-pages/man5/crontab.5.html
 #To send notification to a slack channel ---> https://api.slack.com/tutorials/slack-apps-hello-world
 # by mouhamedfd
 #---------------------------------------------------------------------------------
 #database name
-dbname=
+dumpname=
 #Full path of this script
 spath=
 #create the dumps folder if it doesnt exist
@@ -13,21 +13,23 @@ if [ ! -e $spath/dumps ]
  then
  mkdir -p $spath/dumps
 fi
+#website or app path to be compressed
+webpath=
 #Name of the remote in rclone
 rclone_remote_name=
 #The filename
-filename=dumps\/dump-$(date +'%Y-%m-%dT%H:%M:%S')-$dbname-database.sql
+filename=dumps\/dump-$(date +'%Y-%m-%dT%H:%M:%S')-$dumpname-app.tar.gz
 #For use to notify a slack channel about the operation
 slack_webhook_url=
-#Dumping database using the configuration file my.cnf that contains user and password
-mysqldump --defaults-extra-file=$spath/my.cnf $dbname > $spath/$filename 2>> $spath/error.log
+#dumping website or app
+tar cvzf $filename $webpath
 #sending the dumps to the cloud
-rclone copy  $spath/dumps $rclone_remote_name:mysqldump 2>> $spath/rclone-error.log
+rclone copy  $spath/dumps $rclone_remote_name:webdump 2>> $spath/rclone-error.log
 #Get some informations
-info=$(du -sh $spath)
+info=$(du -sh $spath/dumps)
 nb_files=$(ls -1 "$spath/dumps"|wc -l)
 info="$info $nb_files files"
 #Notify Slack channel
-curl -X POST -H 'Content-type: application/json' --data-binary '{"text":"New DB Backup '"$info"' "}' $slack_webhook_url
+curl -X POST -H 'Content-type: application/json' --data-binary '{"text":"New Website Backup '"$info"' "}' $slack_webhook_url
 
 #---------------------------------------------------------------------------------
