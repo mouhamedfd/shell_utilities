44c27e584978d261424aed48fa1524e2dcc3f10a
Mouhamed Fadel DIAGANA <mouhamedfd@gmail.com>
2021-03-20 01:38:07 +0000

    add userpassword and ownerpassword 	modifié :         wkhtmltopdfprotect.py

--- wkhtmltopdfprotect.py
+++ wkhtmltopdfprotect.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-# script to generate pdf using wkhtmltopdf and password protect the pdf using qpdf
+# script to generate pdf using wkhtmltopdf and userpassword protect the pdf using qpdf
 # Author mouhamedfd http://github.com/mouhamedfd
 # Version 0.2 date 2021-March
 # to be use with pdf generator
 # Example
 # https://github.com/KnpLabs/snappy or https://github.com/barryvdh/laravel-snappy
 # $snappy = new Pdf();
-# $snappy->setBinary('/usr/local/bin/wkhtmltopdfprotect.py password');
+# $snappy->setBinary('/usr/local/bin/wkhtmltopdfprotect.py userpassword ownerpassword');
+# \PDF::setBinary('/usr/local/bin/wkhtmltopdfprotect.py userpassword ownerpassword')
 import os
 import sys
 import shutil
@@ -16,8 +17,9 @@ keep_backup_file=False
 wkhtmltopdf_path="/usr/local/bin/wkhtmltopdf"
 qpdf_path="/usr/bin/qpdf"
 #-------------------------------------------------------
-password=sys.argv[1]
-arguments=sys.argv[2:] #get arguments for wkhtml
+userpassword=sys.argv[1]
+ownerpassword=sys.argv[2]
+arguments=sys.argv[3:] #get arguments for wkhtml
 output_file=arguments[len(arguments)-1]
 base_path=os.path.splitext(output_file)[0]
 backup_output_file=base_path+".bak.pdf"
@@ -42,7 +44,7 @@ shutil.copy2(output_file, backup_output_file)
 #-------------------------------------------------------
 #password protect the file
 try:
-    out_qpdf = Popen([qpdf_path,'--encrypt' ,password ,password ,'256', '--use-aes=y','--',backup_output_file,output_file], stdout=PIPE)
+    out_qpdf = Popen([qpdf_path,'--encrypt' ,userpassword ,ownerpassword ,'256', '--use-aes=y','--',backup_output_file,output_file], stdout=PIPE)
     stdout_qpdf, stderr_qpdf = out_qpdf.communicate()
     exit_code_qpdf=out_qpdf.returncode
 except CalledProcessError as e:
