1476b2c71b25d90b2bcfd2beb0594b5c26476689
Mouhamed Fadel DIAGANA <mouhamedfd@gmail.com>
2022-06-30 03:17:30 +0000

    Add monthly backup folder 	modifié :         dump_database/dump.sh

--- dump_database/dump.sh
+++ dump_database/dump.sh
@@ -8,15 +8,21 @@
 dbname=
 #Full path of this script
 spath=
+month_folder=Backup-$(date +"%Y-%m")
 #create the dumps folder if it doesnt exist
 if [ ! -e $spath/dumps ]
  then
  mkdir -p $spath/dumps
 fi
+#create the monthly dumps folder if it doesnt exist
+if [ ! -e $spath/dumps\/$month_folder ]
+ then
+ mkdir -p $spath/dumps\/$month_folder
+fi
 #Name of the remote in rclone
 rclone_remote_name=
 #The filename
-filename=dumps\/dump-$(date +'%Y-%m-%dT%H:%M:%S')-$dbname-database.sql
+filename=dumps\/$month_folder\/dump-$(date +'%Y-%m-%dT%H:%M:%S')-$dbname-database.sql
 #For use to notify a slack channel about the operation
 slack_webhook_url=
 #Dumping database using the configuration file my.cnf that contains user and password
@@ -25,8 +31,9 @@ mysqldump --defaults-extra-file=$spath/my.cnf $dbname > $spath/$filename 2>> $sp
 rclone copy  $spath/dumps $rclone_remote_name:mysqldump 2>> $spath/rclone-error.log
 #Get some informations
 info=$(du -sh $spath)
-nb_files=$(ls -1 "$spath/dumps"|wc -l)
-info="$info $nb_files files"
+#nb_files=$(ls -1 "$spath/dumps"|wc -l)
+nb_files=$(tree "$spath/dumps"|tail -n1)
+info="$info $nb_files"
 #Notify Slack channel
 curl -X POST -H 'Content-type: application/json' --data-binary '{"text":"New DB Backup '"$info"' "}' $slack_webhook_url
 
